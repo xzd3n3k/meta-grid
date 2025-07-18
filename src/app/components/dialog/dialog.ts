0d618323af1100aa8861e2979b4605bd79d226a6
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-07-18 21:03:17 +0200

    feat(Dialog): create dialog component

---
+++ src/app/components/dialog/dialog.ts
@@ -0,0 +1,35 @@
+import { Component, ElementRef, input, output, signal, viewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog',
+  imports: [],
+  templateUrl: './dialog.html',
+  styleUrl: './dialog.scss'
+})
+export class Dialog {
+  readonly backdrop = input(true);
+  readonly closable = input(true);
+
+  readonly closed = output();
+
+  private readonly dialogRef = viewChild.required<ElementRef<HTMLDivElement>>('dialog');
+
+  protected readonly opened = signal(false);
+
+  onBackdropClick(event: MouseEvent) {
+    if (this.closable() && (event.target as HTMLElement).classList.contains('dialog-backdrop')) {
+      this.close();
+    }
+  }
+
+  open() {
+    this.opened.set(true);
+  }
+
+  close() {
+    if (this.closable()) {
+      this.opened.set(false);
+      this.closed.emit();
+    }
+  }
+}
