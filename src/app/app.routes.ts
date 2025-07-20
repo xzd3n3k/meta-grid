f96412dd8e1a64fbe030db4f0d4982f5dd868672
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-07-20 17:46:02 +0200

    feat(App): add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,17 @@ import {AuthGuard} from '@angular/fire/auth-guard';
 import {Logout} from './components/logout/logout';
 import {Home} from './components/home/home';
 import {MemberTypeTable} from './components/member-type-table/member-type-table';
+import {MemberTable} from './components/member-table/member-table';
+import {MemberForm} from './components/member-form/member-form';
 
 export const routes: Routes = [
   { path: 'login', component: AuthForm },
   { path: 'logout', component: Logout },
+  { path: 'novy-clen', component: MemberForm, canActivate: [AuthGuard] },
   { path: 'novy-typ-clena', component: MemberTypeForm, canActivate: [AuthGuard] },
   { path: 'spravovat-typy-clenu', component: MemberTypeTable, canActivate: [AuthGuard] },
   { path: 'domu', component: Home, canActivate: [AuthGuard] },
+  { path: 'tabulka/:id', component: MemberTable, canActivate: [AuthGuard] },
   { path: '', redirectTo: 'domu', pathMatch: 'full' },
   { path: '**', redirectTo: 'domu' },
 ];
