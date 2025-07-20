3e0e87027ed52e7b0b0ce03a1083b4a8e4c327bd
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-07-20 17:42:48 +0200

    feat(Home): clear component

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,38 +1,9 @@
-import {Component, inject, signal} from '@angular/core';
-import {MemberForm} from '../member-form/member-form';
-import {MemberTable} from '../member-table/member-table';
-import {FormsModule} from '@angular/forms';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'hxt-home',
-  imports: [
-    MemberForm,
-    MemberTable,
-    FormsModule
-  ],
+  imports: [],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {
-  private readonly router = inject(Router);
-
-  protected readonly showMemberForm = signal(false);
-
-  protected addMember() {
-    this.showMemberForm.set(true);
-  }
-
-  protected addMemberType() {
-    this.router.navigate(['novy-typ-clena']);
-  }
-
-  protected manageMemberTypes() {
-    this.router.navigate(['spravovat-typy-clenu']);
-  }
-
-  protected logout() {
-    this.router.navigate(['logout']);
-  }
-
-}
+export class Home {}
