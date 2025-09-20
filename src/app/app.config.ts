d60059f7bbf84dd826f56c6525b20b73e548a01d
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-09-20 20:15:51 +0200

    fix(config): add provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,12 +7,14 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { firebaseConfig } from './firebase/firebase.config';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
