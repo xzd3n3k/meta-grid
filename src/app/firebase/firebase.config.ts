9eb40a4aab1eea7920fbdeeb328198d6c31c8860
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-09-20 19:47:50 +0200

    chore(FirebaseConfig): add firebase config

---
+++ src/app/firebase/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZas1ZI9bbpINJyE9sYkNeiKBXjraakOE",
+  authDomain: "table-db-81927.firebaseapp.com",
+  projectId: "table-db-81927",
+  storageBucket: "table-db-81927.firebasestorage.app",
+  messagingSenderId: "532132789246",
+  appId: "1:532132789246:web:48d6762e1a0c301d9f9eaf"
+};
