dd3cdde52556bbf6c1456e52c9894cce80537ecf
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-09-24 09:30:22 +0200

    fix(app-routes): redirect to login when unauthorized

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,27 @@
 import { Routes } from '@angular/router';
 import {AuthForm} from './components/auth-form/auth-form';
 import {MemberTypeForm} from './components/member-type-form/member-type-form';
-import {AuthGuard} from '@angular/fire/auth-guard';
+import {AuthGuard, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
 import {Logout} from './components/logout/logout';
 import {Home} from './components/home/home';
 import {MemberTypeTable} from './components/member-type-table/member-type-table';
 import {MemberTable} from './components/member-table/member-table';
 import {MemberForm} from './components/member-form/member-form';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const authGuard = {
+  canActivate: [AuthGuard],
+  data: { authGuardPipe: redirectUnauthorizedToLogin }
+};
+
 export const routes: Routes = [
   { path: 'login', component: AuthForm },
   { path: 'logout', component: Logout },
-  { path: 'novy-clen', component: MemberForm, canActivate: [AuthGuard] },
-  { path: 'novy-typ-clena', component: MemberTypeForm, canActivate: [AuthGuard] },
-  { path: 'spravovat-typy-clenu', component: MemberTypeTable, canActivate: [AuthGuard] },
-  { path: 'domu', component: Home, canActivate: [AuthGuard] },
-  { path: 'tabulka/:id', component: MemberTable, canActivate: [AuthGuard] },
+  { path: 'novy-clen', component: MemberForm, ...authGuard },
+  { path: 'novy-typ-clena', component: MemberTypeForm, ...authGuard },
+  { path: 'spravovat-typy-clenu', component: MemberTypeTable, ...authGuard },
+  { path: 'domu', component: Home, ...authGuard },
+  { path: 'tabulka/:id', component: MemberTable, ...authGuard },
   { path: '', redirectTo: 'domu', pathMatch: 'full' },
   { path: '**', redirectTo: 'domu' },
 ];
